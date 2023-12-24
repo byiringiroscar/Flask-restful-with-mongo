12227861a127e8deb4e5b14d01bdf35749c87c53
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-25 00:06:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 
 app = Flask(__name__)
@@ -14,13 +14,27 @@ todos  = {
 
 }
 
+task_post_args = reqparse.RequestParser()
+task_post_args.add_argument('task', type=str, help='Task is required', required=True)
+task_post_args.add_argument('summary', type=str, help='Summary is required', required=True)
+
 class TodosList(Resource):
     def get(self):
         return todos
+    
+    def post(self):
+        args = task_post_args.parse_args()
+        todo_id = int(max(todos.keys())) + 1
+        todos[todo_id] = {
+            'task': args['task'],
+            'summary': args['summary']
+        }
+        return todos[todo_id], 201
 
 class ToDo(Resource):
     def get(self, todo_id):
         return {todo_id: todos[todo_id]}
+    
     
 
 api.add_resource(ToDo,  '/todos/<int:todo_id>')
